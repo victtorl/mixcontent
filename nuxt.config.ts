4e1a7615098b469cb9248ca47d582a8e65a086a3
victtorl <lince00@outlook.com>
2025-02-03 23:52:47 -0500

    resultado de una promesa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  modules: ['nuxt-shiki'],
   css: ['~/assets/css/main.css'],
   vite: {
     plugins: [
--- pages/blogs/resultado-de-una-promesa.vue
+++ pages/blogs/resultado-de-una-promesa.vue
@@ -6,7 +6,7 @@
             <div class="">
 
                 <a href="#" class="text-indigo-600 hover:text-gray-700 transition duration-500 ease-in-out text-sm">
-                    Election
+                    Javascript
                 </a>
                 <h1 href="#" class="text-gray-900 font-bold text-4xl">Como usar el resultado de una promesa</h1>
                 <div class="py-5 text-sm font-regular text-gray-900 flex">
@@ -16,7 +16,7 @@
 		<path d="M256,0C114.837,0,0,114.837,0,256s114.837,256,256,256s256-114.837,256-256S397.163,0,256,0z M277.333,256
 			c0,11.797-9.536,21.333-21.333,21.333h-85.333c-11.797,0-21.333-9.536-21.333-21.333s9.536-21.333,21.333-21.333h64v-128
 		                   	c0-11.797,9.536-21.333,21.333-21.333s21.333,9.536,21.333,21.333V256z"></path></g></g></svg>
-                      <span class="ml-1">6 mins ago</span>
+                      <span class="ml-1">4 mins </span>
                     </span>
 
                     <a href="#" class="flex flex-row items-center hover:text-indigo-600  mr-3">
@@ -27,7 +27,7 @@
                             </path>
                             <path d="M0 0h24v24H0z" fill="none"></path>
                         </svg>
-                        <span class="ml-1">AliSher Azimi</span></a>
+                        <span class="ml-1">Victor Lara</span></a>
 
                     <a href="#" class="flex flex-row items-center hover:text-indigo-600">
                         <svg class="text-indigo-600" fill="currentColor" height="16px" aria-hidden="true" role="img"
@@ -36,63 +36,62 @@
                                 d="M15.4496399,8.42490555 L8.66109799,1.63636364 L1.63636364,1.63636364 L1.63636364,8.66081885 L8.42522727,15.44178 C8.57869221,15.5954158 8.78693789,15.6817418 9.00409091,15.6817418 C9.22124393,15.6817418 9.42948961,15.5954158 9.58327627,15.4414581 L15.4486339,9.57610048 C15.7651495,9.25692435 15.7649133,8.74206554 15.4496399,8.42490555 Z M16.6084423,10.7304545 L10.7406818,16.59822 C10.280287,17.0591273 9.65554997,17.3181054 9.00409091,17.3181054 C8.35263185,17.3181054 7.72789481,17.0591273 7.26815877,16.5988788 L0.239976954,9.57887876 C0.0863319284,9.4254126 0,9.21716044 0,9 L0,0.818181818 C0,0.366312477 0.366312477,0 0.818181818,0 L9,0 C9.21699531,0 9.42510306,0.0862010512 9.57854191,0.239639906 L16.6084423,7.26954545 C17.5601275,8.22691012 17.5601275,9.77308988 16.6084423,10.7304545 Z M5,6 C4.44771525,6 4,5.55228475 4,5 C4,4.44771525 4.44771525,4 5,4 C5.55228475,4 6,4.44771525 6,5 C6,5.55228475 5.55228475,6 5,6 Z">
                             </path>
                         </svg>
-                        <span class="ml-1">activewear</span></a>
+                        <span class="ml-1">
+                        03/02/2025
+                        </span></a>
                 </div>
                 <hr>
                 <p class="text-base leading-8 my-5">
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                    industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
-                    and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
-                    leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
-                    with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
-                    publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                    Muchas veces es necesario gestionar el resultado de un promesa en una funcion
+                    ya sea que la devolucion de la promesa necesite ser usada al resolverla
+                    es ahi donde podemos usar la siguiente estructura para gestionar el resultado
+                    de una promesa.
+
                 </p>
 
-                <h3 class="text-2xl font-bold my-5">#1. What is Lorem Ipsum?</h3>
+                <Shiki lang="js" 
+                code="
+//USAR  DE INMEDIDTO LOS RESULTADOS DE UNA PROMESA 
+const PromesaGood=() => {
+const nuevo = new Promise(res => {
+    res(ismatchName(nombre.value))   //ismatchname es una promesa que devuelve algo
+})
+nuevo.then(
+    resu => console.log('hacer algon con'+resu)    //aqui podemos usar resu qeu es el resultao de la promesa
+)
+.catch((err => console.log(err))) 
+}
+                " />
+
+                <h3 class="text-2xl font-bold my-5"> ¿Como lo hacemos?</h3>
 
                 <p class="text-base leading-8 my-5">
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                    industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
-                    and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
-                    leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
-                    with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
-                    publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                    Definimos la funcion PromesaGood dentro de ella una constante y le asignamos el nombre nuevo
+                    a nuevo le asiganamos la creacion de una nueva promesa y dentro incluimos la promesa que retorna
+                    el valor que deseamos manejar
                 </p>
-
-                <blockquote class="text-md italic leading-8 my-5 p-5 text-indigo-600 font-semibold">Lorem Ipsum is
-                    simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-                    standard dummy text ever since the 1500s</blockquote>
-
+                <Shiki lang="js" 
+                code="
+const nuevo = new Promise(res => {
+    res(ismatchName(nombre.value)) 
+})
+                " />
                 <p class="text-base leading-8 my-5">
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                    industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
-                    and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
-                    leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
-                    with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
-                    publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                    Luego podemos gestionar el valor de la promesa con la palabra reservada then
                 </p>
+                <Shiki lang="js" 
+                code="
+nuevo.then(
+    resu => console.log('hacer algon con'+resu)  
+)
+                " />
+
+                <blockquote class="text-md italic leading-8 my-5 p-5 text-indigo-600 font-semibold">
+                    Si ocurre un error al resolver la promesa se mostrará la sentencia catch o el error
+                </blockquote>
 
 
-                <a href="#"
-                    class="text-xs text-indigo-600 font-medium hover:text-gray-900 transition duration-500 ease-in-out">
-                    #Election
-                </a>,
-                <a href="#"
-                    class="text-xs text-indigo-600 font-medium hover:text-gray-900 transition duration-500 ease-in-out">
-                    #people
-                </a>,
-                <a href="#"
-                    class="text-xs text-indigo-600 font-medium hover:text-gray-900 transition duration-500 ease-in-out">
-                    #Election2020
-                </a>,
-                <a href="#"
-                    class="text-xs text-indigo-600 font-medium hover:text-gray-900 transition duration-500 ease-in-out">
-                    #trump
-                </a>,
-                <a href="#"
-                    class="text-xs text-indigo-600 font-medium hover:text-gray-900 transition duration-500 ease-in-out">
-                    #Joe
-                </a>
+
 
             </div>
 
